b39ea54660cd63dd47b8cd810707cd54953dc9f7
PersonaNormale <domenico.sabatino2002@gmail.com>
2026-02-12 22:20:54 +0100

    Added CPU Register Interface

---
+++ include/gb/cpu/registers.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <cstdint>
+
+namespace gb::cpu {
+
+enum class Reg8 : uint8_t { A, B, C, D, E, H, L };
+enum class Reg16 : uint8_t { AF, BC, DE, HL, SP, PC };
+enum class Flag : uint8_t { Z = 7, N = 6, H = 5, C = 4 };
+
+class Registers final {
+public:
+  void reset() noexcept;
+
+  [[nodiscard]] uint8_t read8(Reg8 reg) const noexcept;
+  void write8(Reg8 reg, uint8_t value) noexcept;
+
+  [[nodiscard]] uint16_t read16(Reg16 r) const noexcept;
+  void write16(Reg16 reg, uint16_t value) noexcept;
+
+  [[nodiscard]] bool flag(Flag flag) const noexcept;
+  void set_flag(Flag flag, bool value) noexcept;
+
+private:
+  uint8_t a_{}, f_{}, b_{}, c_{}, d_{}, e_{}, h_{}, l_{};
+  uint16_t sp_{}, pc_{};
+
+  void sanitize_f() noexcept;
+};
+
+} // namespace gb::cpu
