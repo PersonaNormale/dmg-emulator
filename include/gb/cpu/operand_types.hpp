73336db47634b2a427740f9df091bf06e69541c3
PersonaNormale <domenico.sabatino2002@gmail.com>
2026-02-18 17:05:18 +0100

    Operand Types Checks Added

--- include/gb/cpu/operand_types.hpp
+++ include/gb/cpu/operand_types.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <cassert>
 #include <cstdint>
+
 namespace gb::cpu::operand_types {
 
 enum class Reg8 { B, C, D, E, H, L, MemHL, A };
@@ -12,20 +14,40 @@ enum class Cond { NZ, Z, NC, C };
 
 // Invariant: Value has to be [0...7]
 struct Index3Bit {
-  uint8_t value;
+private:
+  uint8_t val;
+
+public:
+  explicit constexpr Index3Bit(uint8_t x) : val{x} { assert(x < 8); }
+  constexpr uint8_t value() const noexcept { return val; }
 };
 
 // Invariant: Value has to be [0...7]
 struct Target3Bit {
-  uint8_t value;
+private:
+  uint8_t val;
+
+public:
+  explicit constexpr Target3Bit(uint8_t x) : val{x} { assert(x < 8); }
+  constexpr uint8_t value() const noexcept { return val; }
 };
 
 struct Imm8Bit {
-  uint8_t value;
+private:
+  uint8_t val;
+
+public:
+  explicit constexpr Imm8Bit(uint8_t x) : val{x} {}
+  constexpr uint8_t value() const noexcept { return val; }
 };
 
 struct Imm16Bit {
-  uint16_t value;
+private:
+  uint16_t val;
+
+public:
+  explicit constexpr Imm16Bit(uint16_t x) : val{x} {}
+  constexpr uint16_t value() const noexcept { return val; }
 };
 
 } // namespace gb::cpu::operand_types
